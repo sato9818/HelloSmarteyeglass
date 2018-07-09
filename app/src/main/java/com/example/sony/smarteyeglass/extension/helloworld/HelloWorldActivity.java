1a47dbe24863b2402c664e4eb388c0880c026396
sato <kouki9818@gmail.com>
2018-07-09 16:48:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sony/smarteyeglass/extension/helloworld/HelloWorldActivity.java
@@ -0,0 +1,182 @@
+/*
+Copyright (c) 2011, Sony Mobile Communications Inc.
+Copyright (c) 2014, Sony Corporation
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ * Neither the name of the Sony Mobile Communications Inc.
+ nor the names of its contributors may be used to endorse or promote
+ products derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.example.sony.smarteyeglass.extension.helloworld;
+
+import com.sonyericsson.extras.liveware.aef.registration.Registration;
+
+
+import android.location.LocationListener;
+
+import android.app.Activity;
+import android.Manifest;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+
+/**
+ * The Hello World activity provides a button on the phone that starts
+ * the  SmartEyeglass app.
+ *
+ * For demonstration, this also displays messages sent in the intent.
+ */
+public class HelloWorldActivity extends Activity  {
+
+    private final int REQUEST_PERMISSION = 10;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.phonelayout);
+        Log.d("MainActivity", "onCreate()");
+
+
+        if(Build.VERSION.SDK_INT >= 23){
+            checkPermission();
+        }
+        else{
+            locationActivity();
+        }
+        /*
+         * Check if activity was started with a message in the intent
+         * If there is a message, show it as a Toast message
+         */
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String message = extras.getString("Message");
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        /*
+         * Make sure ExtensionService of your SmartEyeglass app has already
+         * started.
+         * This is normally started automatically when user enters your app
+         * on SmartEyeglass, although you can initialize it early using
+         * request intent.
+         */
+
+
+        if (HelloWorldExtensionService.Object == null) {
+            Intent intent = new Intent(Registration.Intents
+                    .EXTENSION_REGISTER_REQUEST_INTENT);
+            Context context = getApplicationContext();
+            intent.setClass(context, HelloWorldExtensionService.class);
+            context.startService(intent);
+        }
+
+
+    }
+
+    // 位置情報許可の確認
+    public void checkPermission() {
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)==
+                PackageManager.PERMISSION_GRANTED){
+
+            locationActivity();
+        }
+        // 拒否していた場合
+        else{
+            requestLocationPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSION);
+
+        } else {
+            Toast toast = Toast.makeText(this,
+                    "許可されないとアプリが実行できません", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},
+                    REQUEST_PERMISSION);
+
+        }
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationActivity();
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this,
+                        "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    // Intent でLocation
+    private void locationActivity() {
+        Intent intent = new Intent(getApplication(), LocationActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sony/smarteyeglass/extension/helloworld/LocationActivity.java
@@ -0,0 +1,335 @@
+package com.example.sony.smarteyeglass.extension.helloworld;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class LocationActivity extends Activity {
+    public static String lati = "取得できません。";
+    public static String longti = "";
+    // Fused Location Provider API.
+    private FusedLocationProviderClient fusedLocationClient;
+
+    // Location Settings APIs.
+    private SettingsClient settingsClient;
+    private LocationSettingsRequest locationSettingsRequest;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private Location location;
+
+    private String lastUpdateTime;
+    private Boolean requestingLocationUpdates;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private int priority = 0;
+    private TextView textView;
+    private String textLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.phonelayout);
+
+
+        fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
+
+        priority = 0;
+
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingsRequest();
+/*
+        textView = (TextView) findViewById(R.id.text_view);
+        textLog = "onCreate()\n";
+        textView.setText(textLog);
+*/
+        // 測位開始
+        Button buttonStart = (Button) findViewById(R.id.button_start);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLocationUpdates();
+            }
+        });
+
+        // 測位終了
+        Button buttonStop = (Button) findViewById(R.id.button_stop);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopLocationUpdates();
+            }
+        });
+
+    }
+
+    // locationのコールバックを受け取る
+    private void createLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                location = locationResult.getLastLocation();
+
+                lastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+                updateLocationUI();
+            }
+        };
+    }
+
+    private void updateLocationUI() {
+        // getLastLocation()からの情報がある場合のみ
+        if (location != null) {
+
+            double d1 = location.getLatitude();
+            double d2 = location.getLongitude();
+            lati = BigDecimal.valueOf(d1).toPlainString();
+            longti = BigDecimal.valueOf(d2).toPlainString();
+
+/*
+            String fusedName[] = {
+                    "Latitude", "Longitude", "Accuracy",
+                    "Altitude", "Speed", "Bearing"
+            };
+
+            double fusedData[] = {
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    location.getAccuracy(),
+                    location.getAltitude(),
+                    location.getSpeed(),
+                    location.getBearing()
+            };
+
+            StringBuilder strBuf =
+                    new StringBuilder("---------- UpdateLocation ---------- \n");
+
+            for(int i=0; i< fusedName.length; i++) {
+                strBuf.append(fusedName[i]);
+                strBuf.append(" = ");
+                strBuf.append(String.valueOf(fusedData[i]));
+                strBuf.append("\n");
+            }
+
+            strBuf.append("Time");
+            strBuf.append(" = ");
+            strBuf.append(lastUpdateTime);
+            strBuf.append("\n");
+
+            textLog += strBuf;
+            textView.setText(textLog);
+*/
+        }
+
+    }
+
+    private void createLocationRequest() {
+        locationRequest = new LocationRequest();
+
+        if (priority == 0) {
+            // 高い精度の位置情報を取得したい場合
+            // インターバルを例えば5000msecに設定すれば
+            // マップアプリのようなリアルタイム測位となる
+            // 主に精度重視のためGPSが優先的に使われる
+            locationRequest.setPriority(
+                    LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        } else if (priority == 1) {
+            // バッテリー消費を抑えたい場合、精度は100mと悪くなる
+            // 主にwifi,電話網での位置情報が主となる
+            // この設定の例としては　setInterval(1時間)、setFastestInterval(1分)
+            locationRequest.setPriority(
+                    LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        } else if (priority == 2) {
+            // バッテリー消費を抑えたい場合、精度は10kmと悪くなる
+            locationRequest.setPriority(
+                    LocationRequest.PRIORITY_LOW_POWER);
+
+        } else {
+            // 受け身的な位置情報取得でアプリが自ら測位せず、
+            // 他のアプリで得られた位置情報は入手できる
+            locationRequest.setPriority(
+                    LocationRequest.PRIORITY_NO_POWER);
+        }
+
+        // アップデートのインターバル期間設定
+        // このインターバルは測位データがない場合はアップデートしません
+        // また状況によってはこの時間よりも長くなることもあり
+        // 必ずしも正確な時間ではありません
+        // 他に同様のアプリが短いインターバルでアップデートしていると
+        // それに影響されインターバルが短くなることがあります。
+        // 単位：msec
+        locationRequest.setInterval(60000);
+        // このインターバル時間は正確です。これより早いアップデートはしません。
+        // 単位：msec
+        locationRequest.setFastestInterval(5000);
+
+    }
+
+    // 端末で測位できる状態か確認する。wifi, GPSなどがOffになっているとエラー情報のダイアログが出る
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder =
+                new LocationSettingsRequest.Builder();
+
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.i("debug", "User agreed to make required location settings changes.");
+                        // Nothing to do. startLocationupdates() gets called in onResume again.
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i("debug", "User chose not to make required location settings changes.");
+                        requestingLocationUpdates = false;
+                        break;
+                }
+                break;
+        }
+    }
+
+
+
+    // FusedLocationApiによるlocation updatesをリクエスト
+    private void startLocationUpdates() {
+        // Begin by checking if the device has the necessary location settings.
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this,
+                        new OnSuccessListener<LocationSettingsResponse>() {
+                            @Override
+                            public void onSuccess(
+                                    LocationSettingsResponse locationSettingsResponse) {
+                                Log.i("debug", "All location settings are satisfied.");
+
+                                // パーミッションの確認
+                                if (ActivityCompat.checkSelfPermission(
+                                        LocationActivity.this,
+                                        Manifest.permission.ACCESS_FINE_LOCATION) !=
+                                        PackageManager.PERMISSION_GRANTED
+                                        && ActivityCompat.checkSelfPermission(
+                                        LocationActivity.this,
+                                        Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                                        PackageManager.PERMISSION_GRANTED) {
+
+                                    // TODO: Consider calling
+                                    //    ActivityCompat#requestPermissions
+                                    // here to request the missing permissions, and then overriding
+                                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                    //                                          int[] grantResults)
+                                    // to handle the case where the user grants the permission. See the documentation
+                                    // for ActivityCompat#requestPermissions for more details.
+                                    return;
+                                }
+                                fusedLocationClient.requestLocationUpdates(
+                                        locationRequest, locationCallback, Looper.myLooper());
+
+                            }
+                        })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i("debug", "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(
+                                            LocationActivity.this,
+                                            REQUEST_CHECK_SETTINGS);
+
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i("debug", "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e("debug", errorMessage);
+                                Toast.makeText(LocationActivity.this,
+                                        errorMessage, Toast.LENGTH_LONG).show();
+
+                                requestingLocationUpdates = false;
+                        }
+
+                    }
+                });
+
+        requestingLocationUpdates = true;
+    }
+
+    private void stopLocationUpdates() {
+        textLog += "onStop()\n";
+        textView.setText(textLog);
+
+        if (!requestingLocationUpdates) {
+            Log.d("debug", "stopLocationUpdates: " +
+                    "updates never requested, no-op.");
+
+
+            return;
+        }
+
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+                .addOnCompleteListener(this,
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                requestingLocationUpdates = false;
+                            }
+                        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // バッテリー消費を鑑みLocation requestを止める
+        stopLocationUpdates();
+    }
+
+}
